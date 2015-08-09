8ce0f115eb5c977d1938f78109193feafd95cac0
Jacob Marshall <git@jacobmarshall.co>
2015-08-09 19:24:45 +1200

    Add markdown property to article object & add timestamp.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var util = require('util'),
     rss = require('parserss'),
     express = require('express'),
+    md = require('to-markdown'),
     app = express();
 
 var svbtle_url = 'https://%s.svbtle.com/feed';
@@ -8,13 +9,17 @@ var svbtle_url = 'https://%s.svbtle.com/feed';
 function parseArticle (article) {
     var title = article.title,
         content = article.description,
+        markdown = md(content),
         author = article.author,
+        timestamp = article.date.valueOf(),
         url = article.link;
 
     return {
         title: title,
         content: content,
+        markdown: markdown,
         author: author,
+        timestamp: timestamp,
         url: url
     };
 }
@@ -45,7 +50,6 @@ app.use(function (req, res, next) {
 });
 
 app.get('/:username/latest', function (req, res) {
-    //console.log(res);
     getRSSFeed(req.params.username, 1, function (err, data) {
         if (handleError(res, err, data)) return;
         res.json(parseArticle(data.articles[0]));
